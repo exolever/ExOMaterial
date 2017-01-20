6cfe37f7d16c1c514a76b9a9ae221d0aca9fe2f0
ana_panadero <ana.panadero@exo.works>
2017-01-20 11:22:39 +0100

    Remove covalent dependencies

--- covalentDependencies.js
+++
@@ -1,5 +0,0 @@
-npm install '@covalent/core@file:./plugins/covalent/core';
-npm install '@covalent/dynamic-forms@file:./plugins/covalent/dynamic-forms';
-npm install '@covalent/highlight@file:./plugins/covalent/highlight';
-npm install '@covalent/http@file:./plugins/covalent/http';
-npm install '@covalent/markdown@file:./plugins/covalent/markdown';
