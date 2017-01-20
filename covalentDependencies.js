88d67e278b027a63716d32a26de657e1a9cff39a
ana_panadero <ana.panadero@exo.works>
2017-01-20 10:37:07 +0100

    Postinstall script for covalent dependencies

---
+++ covalentDependencies.js
@@ -0,0 +1,5 @@
+npm install 'file:./plugins/covalent/core';
+npm install 'file:./plugins/covalent/dynamic-forms';
+npm install 'file:./plugins/covalent/highlight';
+npm install 'file:./plugins/covalent/http';
+npm install 'file:./plugins/covalent/markdown';
